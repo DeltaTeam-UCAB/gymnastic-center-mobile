9f1343325d502cb2ebc7367763a9b18da271d776
HappyGick <egarciayustiz@gmail.com>
2024-05-04 14:07:28 -0400

    feat(router): :construction: add welcome screen and splash routes

--- lib/presentation/core/router/app_router.dart
+++ lib/presentation/core/router/app_router.dart
@@ -1,12 +1,21 @@
 import 'package:go_router/go_router.dart';
 import 'package:gymnastic_center/presentation/screens/home_screen.dart';
+import 'package:gymnastic_center/presentation/screens/splash_screen.dart';
 import 'package:gymnastic_center/presentation/screens/welcome_screen.dart';
 
 class RoutesManager {
   static GoRouter appRouter = GoRouter(routes: [
     GoRoute(
       path: '/',
-      builder: (context, state) => const WelcomeScreen(),
+      builder: (context, state) => const HomeScreen(),
     ),
+    GoRoute(
+      path: '/splash',
+      builder: (context, state) => const SplashScreen(),
+    ),
+    GoRoute(
+      path: '/welcome',
+      builder: (context, state) => const WelcomeScreen(),
+    )
   ]);
 }
