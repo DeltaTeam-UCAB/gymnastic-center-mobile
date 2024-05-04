9d9029a133a5885152bcb210dd486571011f5cc7
manuelenrq9 <manuel.enrqt@gmail.com>
2024-05-04 00:33:13 -0400

    start_screen added
    start_screen added

--- lib/presentation/core/router/app_router.dart
+++ lib/presentation/core/router/app_router.dart
@@ -1,16 +1,16 @@
-
 import 'package:go_router/go_router.dart';
+import 'package:gymnastic_center/presentation/screens/auth/start_screen.dart';
 import 'package:gymnastic_center/presentation/screens/home_screen.dart';
 
-
 class RoutesManager {
-  static GoRouter appRouter = GoRouter(
-    routes: [
-      GoRoute(
-        path: '/',
-        builder: (context, state) => const HomeScreen(),
-      ),
-
-    ]
-  );
+  static GoRouter appRouter = GoRouter(routes: [
+    GoRoute(
+      path: '/',
+      builder: (context, state) => const HomeScreen(),
+    ),
+    GoRoute(
+      path: '/test',
+      builder: (context, state) => const StartScreen(),
+    ),
+  ]);
 }
