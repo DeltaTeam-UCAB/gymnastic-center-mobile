61c5e28927f30c0d340eb62fe6720274281d8725
Alejandro <alejoguevarafm@gmail.com>
2024-05-05 16:03:19 -0400

    feat: :sparkles: Settings Screen + Side Menu
    Se realizo la pantalla de settings junto con el sidebar haciendo uso de una appMenuItems donde se indican los campos que saldran incluyendo el icono, titulo y la ruta a donde iran

---
+++ lib/presentation/core/menu/menu_items.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+
+class MenuItem {
+  final String title;
+  final String route;
+  final IconData icon;
+
+  const MenuItem({
+    required this.title,
+    required this.route,
+    required this.icon,
+  });
+}
+
+const appMenuItems = <MenuItem>[
+    MenuItem(
+        title: 'Account',
+        route: '/account',
+        icon: Icons.account_circle_rounded,
+    ),
+
+    MenuItem(
+      title: 'Notifications',
+      route: '/notifications',
+      icon: Icons.notifications_rounded,
+    ),
+
+    MenuItem(
+      title: 'Privacy',
+      route: '/privacy',
+      icon: Icons.lock_rounded,
+    ),
+
+    MenuItem(
+      title: 'FAQ',
+      route: '/faq',
+      icon: Icons.question_mark_rounded,
+    ),
+
+    MenuItem(
+      title: 'Language',
+      route: '/language',
+      icon: Icons.language_rounded,
+    ),
+
+    MenuItem(
+      title: 'Rate Us',
+      route: '/rate-us',
+      icon: Icons.star_rounded,
+    ),
+
+    MenuItem(
+      title: 'About',
+      route: '/about',
+      icon: Icons.info_rounded,
+    ),
+];
