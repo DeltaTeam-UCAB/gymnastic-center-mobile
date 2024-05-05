152c0ba134c97e81688abbe9ace3bd422b7a3ce0
Alejandro <alejoguevarafm@gmail.com>
2024-05-05 16:21:31 -0400

    feat: :sparkles: Home Navigation
    Se realizo la configuración de la navegación principal en el home_screen de esta manera se puede manejar usando el bottom navigation

---
+++ lib/presentation/screens/screens.dart
@@ -0,0 +1,2 @@
+export 'package:gymnastic_center/presentation/screens/home_screen.dart';
+
--- lib/presentation/screens/tabs/notifications_screen.dart
+++ lib/presentation/screens/tabs/notifications_screen.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:gymnastic_center/infrastructure/models/push_message_model.dart';
-import 'package:gymnastic_center/presentation/widgets/notifications/notifications_listview.dart';
+import 'package:gymnastic_center/presentation/screens/tabs/widgets/notifications_listview.dart';
 
 class NotificationsScreen extends StatelessWidget {
 
