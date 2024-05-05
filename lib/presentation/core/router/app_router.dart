53b9169897085c009d52b18781b0820b3b3ffafd
Alejandro <alejoguevarafm@gmail.com>
2024-05-05 16:28:06 -0400

    perf: :zap: Configuration Navigation
    Se arreglo para que se pueda navegar entre varias pestañas y luego en las routes hijas iran las rutas hijas

--- lib/presentation/core/router/app_router.dart
+++ lib/presentation/core/router/app_router.dart
@@ -1,16 +1,18 @@
-
 import 'package:go_router/go_router.dart';
-import 'package:gymnastic_center/presentation/screens/home_screen.dart';
-
+import 'package:gymnastic_center/presentation/screens/screens.dart';
 
 class RoutesManager {
   static GoRouter appRouter = GoRouter(
+    initialLocation: '/home/0',
     routes: [
       GoRoute(
-        path: '/',
-        builder: (context, state) => const HomeScreen(),
+        path: '/home/:page',
+        builder: (context, state){
+          final pageIndex = int.parse(state.pathParameters['page'] ?? '0');
+          return HomeScreen(pageIndex: pageIndex );
+        },
+        // routes: [] TODO: Add nested routes here.
       ),
-
     ]
   );
 }
