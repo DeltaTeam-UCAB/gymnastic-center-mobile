92178f03fb349ea46ca675cf7cf860a004b6c706
Alejandro <alejoguevarafm@gmail.com>
2024-05-06 22:33:03 -0400

    perf: :zap: Details Course - connection
    Se realizo exitosamente la conexión del back con el detalle de curso. :)

---
+++ lib/presentation/screens/courses/widgets/course_details_view.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+import 'package:gymnastic_center/domain/entities/courses/course.dart';
+import 'package:gymnastic_center/presentation/screens/courses/widgets/custom_icon.dart';
+import 'package:gymnastic_center/presentation/screens/courses/widgets/custom_image.dart';
+import 'package:gymnastic_center/presentation/widgets/courses/videos_courses_listview.dart';
+
+class CourseDetailsView extends StatelessWidget {
+
+  final Course course;
+  const CourseDetailsView({super.key, required this.course});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        CustomImage(
+          src: course.image,
+          instructor: course.instructor,
+          title: course.title,
+        ),
+
+        // Course Details
+        Padding(
+          padding: const EdgeInsets.all(20),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                course.description,
+                textAlign: TextAlign.justify,
+                style: const TextStyle(fontSize: 12)
+                ), 
+            ],
+          ),
+        ),
+
+        const Divider(),
+
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            CustomIcon(icon: Icons.menu, title: 'Calories', subtitle: course.calories),
+            const CustomIcon(
+                icon: Icons.calendar_month_outlined,
+                title: 'Weeks',
+                subtitle: '3'),
+            const CustomIcon(
+                icon: Icons.watch_later_outlined,
+                title: 'Mins',
+                subtitle: '22'),
+          ],
+        ),
+
+        VideosCoursesListView(
+          lessons: course.lessons!,
+        ),
+
+        const SizedBox(height: 100),
+      ],
+    );
+  }
+}
---
+++ lib/presentation/screens/courses/widgets/custom_icon.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+
+class CustomIcon extends StatelessWidget {
+  final IconData icon;
+  final String title;
+  final String subtitle;
+  
+  const CustomIcon(
+      {super.key,
+      required this.icon,
+      required this.title,
+      required this.subtitle});
+
+  @override
+  Widget build(BuildContext context) {
+    final themeColor = Theme.of(context).primaryColor;
+    final textStyles = Theme.of(context).textTheme;
+    return Row(
+      children: [
+        Container(
+          width: 40,
+          height: 40,
+          decoration: BoxDecoration(
+            color: themeColor.withOpacity(0.2),
+            shape: BoxShape.rectangle,
+            borderRadius: BorderRadius.circular(10),
+          ),
+          child: Center(
+            child: Icon(icon, color: themeColor),
+          ),
+        ),
+        const SizedBox(width: 5),
+        Column(
+          children: [
+            //Title
+            Text(title, style: textStyles.labelSmall),
+
+            //Subtitle
+            Text(
+              subtitle,
+              style: textStyles.labelSmall,
+            )
+          ],
+        )
+      ],
+    );
+  }
+}
