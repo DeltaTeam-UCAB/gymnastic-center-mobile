53fb0ce45a471038b70e732cf2fb8839dff616df
Manuel Tirado <65634081+manuelenrq9@users.noreply.github.com>
2024-05-08 19:26:59 -0400

    Delete lib/tempCodeRunnerFile.dart

--- lib/tempCodeRunnerFile.dart
+++
@@ -1,9 +0,0 @@
-sManager.onBackgroundMessage(
-      firebaseMessagingBackgroundHandler);
-
-  // Initialize Firebase, and pass the default options (firebase_options.dart)
-  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
-
-  // Initialize the local notifications
-  await LocalNotifications().initializeLocalNotifications();
-  await Environment.
