bcb70857ffe9d1b6dee8798275c8f023e90b5d79
Gianfranco <gianlanza10@gmail.com>
2024-05-09 21:38:30 -0400

    feat: :necktie: Added Logic in Login Form
    Se creo el bloc para el manejo del formulario de login, OJO el bloc en un futuro debera manejar las validaciones de los campos, pero por ahora solo manejara los mensajes de error que arroja el back como el wrong credentials

---
+++ lib/application/auth/login/login_bloc.dart
@@ -0,0 +1,93 @@
+import 'dart:ffi';
+
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:equatable/equatable.dart';
+import 'package:gymnastic_center/domain/repositories/user/user_repository.dart';
+
+
+part 'login_event.dart';
+part 'login_state.dart';
+
+class LoginBloc extends Bloc<LoginEvent, LoginState> {
+
+  final UserRepository userRespository; 
+
+  LoginBloc({required this.userRespository}) : super(const LoginState()) {
+    on<EmailChanged>(_onEmailChanged);
+    on<PasswordChanged>(_onPasswordChanged);
+    on<ErrorOccurred  >(_onErrorOcurred);
+    on<LoginFormSubmited>(_onFormSubmited);
+    on<LoginCompleted>(_onLoginCompleted);
+
+  }
+  
+  void _onLoginCompleted(LoginCompleted event, Emitter<LoginState> emit){
+    emit(
+      state.copyWith(
+        formStatus: LoginFormStatus.valid,
+      )
+    );
+  }
+
+  void _onFormSubmited(LoginFormSubmited event, Emitter<LoginState> emit){
+    emit(
+      state.copyWith(
+        formStatus: LoginFormStatus.posting,
+      )
+    );
+  }
+
+  void _onEmailChanged(EmailChanged event, Emitter<LoginState> emit){
+    emit(
+      state.copyWith(
+        email: event.email
+      )
+    );
+  }
+
+  void _onPasswordChanged(PasswordChanged event, Emitter<LoginState> emit){
+    emit(
+      state.copyWith(
+        password: event.password
+      )
+    );
+  }
+
+  void _onErrorOcurred(ErrorOccurred event, Emitter<LoginState> emit){
+    emit(
+      state.copyWith(
+        formStatus: LoginFormStatus.invalid,
+        errorMessage: event.errorMessage
+      )
+    );
+  }
+
+  void changeEmail(String email){
+    add(EmailChanged(email: email));
+  }
+
+  void changePassword(String password){
+    add(PasswordChanged(password: password));
+  }
+
+
+  Future<void> submit() async{
+    add(LoginFormSubmited());
+    if ( state.email == '' || state.password == '') {
+      add(ErrorOccurred(errorMessage: 'Invalid fields. You must enter your credentials'));
+      return;
+    }
+    final isLoggedResult = await userRespository.login(state.email, state.password);
+    if ( isLoggedResult.isSuccessful() ){
+      final isLogged = isLoggedResult.getValue();
+      if ( isLogged ){
+        print('is logged' +  isLogged.toString());
+        add(LoginCompleted());
+        return ; 
+      }
+    }
+    add(ErrorOccurred(errorMessage: isLoggedResult.getError().toString()));
+
+
+  }
+}
---
+++ lib/application/auth/login/login_state.dart
@@ -0,0 +1,35 @@
+part of 'login_bloc.dart';
+
+enum LoginFormStatus { invalid, valid, validating, posting }
+
+class LoginState extends Equatable {
+  final String password;
+  final String email;
+  final String errorMessage;
+  final LoginFormStatus formStatus;
+
+  const LoginState({
+    this.password = '', 
+    this.email = '', 
+    this.formStatus = LoginFormStatus.invalid,
+    this.errorMessage = '',
+  });
+
+  LoginState copyWith({
+    String? email,
+    String? password,
+    String? errorMessage,
+    LoginFormStatus? formStatus
+  }) => LoginState(
+    email: email ?? this.email,
+    password: password ?? this.password,
+    errorMessage: errorMessage ?? this.errorMessage,
+    formStatus: formStatus ?? this.formStatus,
+  );
+
+
+
+  @override
+  List<Object> get props => [email, password, formStatus, errorMessage];
+}
+
