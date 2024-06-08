1e8416d089cc95c357aa381a6305155af4098286
Alejandro <alejoguevarafm@gmail.com>
2024-06-08 16:11:35 -0400

    refactor: :fire: Delete Client & Videos
    Se eliminaron todo lo relacionado a cliente y de video se elimino el repository y datasource

--- lib/domain/repositories/videos/videos_repository.dart
+++
@@ -1,6 +0,0 @@
-import 'package:gymnastic_center/common/results.dart';
-import 'package:gymnastic_center/domain/entities/videos/video.dart';
-
-abstract class VideosRepository {
-  Future<Result<Video>> getVideoById(String id);
-}
