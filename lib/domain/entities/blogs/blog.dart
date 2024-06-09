80a213a618062862585fb21856762a813b244d25
Alejandro <alejoguevarafm@gmail.com>
2024-06-08 23:00:20 -0400

    refactor: :recycle: Domain Refactor Blog
    Se actualizo la entidad así como el datasource y repository

--- lib/domain/entities/blogs/blog.dart
+++ lib/domain/entities/blogs/blog.dart
@@ -1,18 +1,23 @@
-class Post {
+import 'package:gymnastic_center/domain/entities/trainers/trainer.dart';
+
+class Blog {
   final String id;
   final String title;
   final String body;
   final DateTime released;
   final List<String> images;
-  final String autor;
+  final Trainer trainer;
   final List<String> tags;
+  final String category;
 
-  const Post(
+
+  const Blog(
       {required this.id,
       required this.title,
       required this.body,
       required this.released,
       required this.images,
-      required this.autor,
+      required this.trainer,
+      required this.category,
       required this.tags});
 }
