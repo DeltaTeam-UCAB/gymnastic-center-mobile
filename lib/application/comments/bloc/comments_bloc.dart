dbec567a80ba36e27e46ea2c9e658c1f229ae8da
Gianfranco <gianlanza10@gmail.com>
2024-06-11 15:44:12 -0400

    feat: :sparkles: Added CommentsBloc
    Se agrego el bloc con todas las funcionalidad de los comentarios: Like, Dislike, Cargar comentarios de forma paginada, postear comentarios

--- lib/application/comments/bloc/comments_bloc.dart
+++ lib/application/comments/bloc/comments_bloc.dart
@@ -12,18 +12,39 @@ class CommentsBloc extends Bloc<CommentsEvent, CommentsState> {
 
   CommentsBloc(this.commentsRepository) : super(const CommentsState()) {
     on<CommentsLoaded>(_onCommentsLoaded);
-    on<CommentLiked>(_onCommentLiked);
-    on<CommentDisliked>(_onCommentDisliked);
-
+    on<CommentLikesChanged>(_onCommentLikesChanged);
+    on<CommentDiskesChanged>(_onCommentDiskesChanged);
     on<ErrorOccurred>(_onErrorOccurred);
-    on<CommentsCompleted>(_onCommentsCompleted);
+    on<AllCommentsLoaded>(_onAllCommentsLoaded);
+    on<CommentsReset>(_onCommentsReset);
     on<LoadingStarted>(_onLoadingStarted);
+    on<CommentPosted>(_onCommentPosted);
+    on<CommentPostingStarted>(_onCommentPostingStarted);
+  }
+
+
+  void _onCommentPosted(CommentPosted event, Emitter<CommentsState> emit){
+    emit(
+      state.copyWith(
+        page: -1,
+        comments: [],
+        isPosting: false
+      )
+    );
+  }
+
+  void _onCommentPostingStarted(CommentPostingStarted event, Emitter<CommentsState> emit){
+    emit(
+      state.copyWith(
+        isPosting: true
+      )
+    );
   }
 
   void _onLoadingStarted(LoadingStarted event, Emitter<CommentsState> emit){
     emit(
       state.copyWith(
-        status: CommentsStatus.error
+        status: CommentsStatus.loading
       )
     );
   }
@@ -36,10 +57,10 @@ class CommentsBloc extends Bloc<CommentsEvent, CommentsState> {
     );
   }
 
-  void _onCommentsCompleted(CommentsCompleted event, Emitter<CommentsState> emit){
+  void _onAllCommentsLoaded(AllCommentsLoaded event, Emitter<CommentsState> emit){
     emit(
       state.copyWith(
-        status: CommentsStatus.completed
+        status: CommentsStatus.allCommentsLoaded
       )
     );
   }
@@ -47,27 +68,55 @@ class CommentsBloc extends Bloc<CommentsEvent, CommentsState> {
   void _onCommentsLoaded(CommentsLoaded event, Emitter<CommentsState> emit){
     emit(
       state.copyWith(
-        offset: state.offset + event.comments.length,
+        page: state.page + 1,
         comments: [...state.comments, ...event.comments],
         status: CommentsStatus.loaded
       )
     );
   }
+
+  void _onCommentsReset(CommentsReset event, Emitter<CommentsState> emit){
+    emit(
+      state.copyWith(
+        page: -1,
+        comments: [],
+        status: CommentsStatus.loaded,
+        isPosting: false
+      )
+    );
+  }
   
-  void _onCommentLiked(CommentLiked event, Emitter<CommentsState> emit){
+  void _onCommentLikesChanged(CommentLikesChanged event, Emitter<CommentsState> emit){
     emit(
       state.copyWith(
         comments: state.comments.map((comment){
           if (comment.id == event.commentId){
+            
+            late int newLikes; 
+            late int newDislikes;
+
+            if ( event.newCommentLikeState ){
+              newLikes = comment.likes + 1;
+              if (comment.userDisliked) { 
+                newDislikes = comment.dislikes - 1;
+              }else{
+                newDislikes = comment.dislikes;
+              }
+            } else {
+              newLikes = comment.likes - 1;
+              newDislikes = comment.dislikes;
+            }
+
             return Comment(
               id: comment.id,
               username: comment.username,
-              clientId: comment.clientId,
-              description: comment.description,
+              body: comment.body,
               creationDate: comment.creationDate,
-              likes: comment.likes + 1,
-              dislikes: comment.dislikes,
-              userLiked: !comment.userLiked);
+              likes: newLikes,
+              dislikes: newDislikes,
+              userLiked: event.newCommentLikeState,
+              userDisliked: false
+            );
           }
           return comment;
         }).toList(),
@@ -75,20 +124,35 @@ class CommentsBloc extends Bloc<CommentsEvent, CommentsState> {
     );
   }
 
-  void _onCommentDisliked(CommentDisliked event, Emitter<CommentsState> emit){
+  void _onCommentDiskesChanged(CommentDiskesChanged event, Emitter<CommentsState> emit){
     emit(
       state.copyWith(
         comments: state.comments.map((comment){
           if (comment.id == event.commentId){
+            late int newLikes; 
+            late int newDislikes;
+
+            if ( event.newCommentDislikeState ){
+              newDislikes = comment.dislikes + 1;
+              if (comment.userLiked) { 
+                newLikes = comment.likes - 1;
+              }else{
+                newLikes = comment.likes;
+              }
+            } else {
+              newLikes = comment.likes;
+              newDislikes = comment.dislikes - 1;
+            }
             return Comment(
               id: comment.id,
               username: comment.username,
-              clientId: comment.clientId,
-              description: comment.description,
+              body: comment.body,
               creationDate: comment.creationDate,
-              likes: comment.likes - 1,
-              dislikes: comment.dislikes,
-              userLiked: !comment.userLiked);
+              likes: newLikes,
+              dislikes: newDislikes,
+              userLiked: false,
+              userDisliked: event.newCommentDislikeState
+            );
           }
           return comment;
         }).toList(),
@@ -96,38 +160,82 @@ class CommentsBloc extends Bloc<CommentsEvent, CommentsState> {
     );
   }
 
-Future<void> deleteLike(String commentId, bool userliked) async{
-  final commentDislikedResult = await commentsRepository.deleteLikeByCommentId(commentId);
-  if (commentDislikedResult.isSuccessful()){
-    add(CommentDisliked(commentId: commentId));
-    return;
+  Future<void> toggleLike(String commentId) async{
+    final commentLikedResult = await commentsRepository.toggleLikeCommentById(commentId);
+    if (commentLikedResult.isSuccessful()){
+      add(CommentLikesChanged(commentId: commentId, newCommentLikeState: commentLikedResult.getValue()));
+      return;
+    }
+    add(ErrorOccurred());
   }
-  add(ErrorOccurred());
-}
 
-Future<void> likeComment(String commentId, bool userliked) async{
-  final commentLikedResult = await commentsRepository.likeCommentById(commentId);
-  if (commentLikedResult.isSuccessful()){
-    add(CommentLiked(commentId: commentId));
-    return;
+  Future<void> toggleDislike(String commentId) async{
+    final commentDislikedResult = await commentsRepository.toggleDislikeCommentById(commentId);
+    if (commentDislikedResult.isSuccessful()){
+      add(CommentDiskesChanged(commentId: commentId, newCommentDislikeState: commentDislikedResult.getValue()));
+      return;
+    }
+    add(ErrorOccurred());
   }
-  add(ErrorOccurred());
-}
 
-Future<void> loadNextPageByPostId(String postId) async{
-  if (state.status == CommentsStatus.loading ||
-        state.status == CommentsStatus.completed) return;
-  add(LoadingStarted());
-  final commentsResult = await commentsRepository.getCommentsByPostId(postId, limit: state.limit, offset: state.offset);
-  if (commentsResult.isSuccessful()){
-    final comments = commentsResult.getValue();
-    if ( comments.isEmpty ){
-      add(CommentsCompleted());
+  Future<void> loadNextPageByBlogId(String blogId) async{
+    if (state.status == CommentsStatus.loading ||
+          state.status == CommentsStatus.allCommentsLoaded) return;
+    add(LoadingStarted());
+    final commentsResult = await commentsRepository.getCommentsByBlogId(blogId, page: state.page + 1);
+    if (commentsResult.isSuccessful()){
+      final comments = commentsResult.getValue();
+      if ( comments.isEmpty ){
+        add(AllCommentsLoaded());
+        return ;
+      }
+      add(CommentsLoaded(comments: comments));
       return ;
     }
-    add(CommentsLoaded(comments: comments));
-    return ;
+    add(ErrorOccurred());
   }
-  add(ErrorOccurred());
-}
+
+  Future<void> loadNextPageByLessonId(String lessonId) async{
+    if (state.status == CommentsStatus.loading ||
+          state.status == CommentsStatus.allCommentsLoaded) return;
+    add(LoadingStarted());
+    final commentsResult = await commentsRepository.getCommentsByLessonId(lessonId, page: state.page + 1);
+    if (commentsResult.isSuccessful()){
+      final comments = commentsResult.getValue();
+      if ( comments.isEmpty ){
+        add(AllCommentsLoaded());
+        return ;
+      }
+      add(CommentsLoaded(comments: comments));
+      return ;
+    }
+    add(ErrorOccurred());
+  }
+
+  Future<void> createCommentByLessonId(String lessonId, String message) async{
+    if ( message.trim().isEmpty ) return ;
+    add(CommentPostingStarted());
+    final createCommentResult = await commentsRepository.createCommentByLessonId(lessonId, message);
+    if (createCommentResult.isSuccessful()){
+      add(CommentPosted());
+      return ;
+    }
+    add(ErrorOccurred());
+  }
+
+  Future<void> createCommentByBlogId(String blogId, String message) async{
+    if ( message.trim().isEmpty ) return ;
+    add(CommentPostingStarted());
+    final createCommentResult = await commentsRepository.createCommentByBlogId(blogId, message);
+    if (createCommentResult.isSuccessful()){
+      add(CommentPosted());
+      return ;
+    }
+    add(ErrorOccurred());
+  }
+
+  void reset(){
+    add(CommentsReset());
+  }
+
 }
