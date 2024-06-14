37a23e56e7a40b0418e5717ac36b10fc0ae01171
Alejandro <alejoguevarafm@gmail.com>
2024-06-13 23:08:30 -0400

    refactor: :zap: camara-gallery-improve
    se mejoro la toma fotos para adaptarlo

--- lib/infrastructure/camara_gallery/camara_gallery_impl.dart
+++ lib/infrastructure/camara_gallery/camara_gallery_impl.dart
@@ -1,10 +1,15 @@
 import 'dart:convert';
+import 'dart:typed_data';
 
+import 'package:flutter/material.dart';
+import 'package:flutter_image_compress/flutter_image_compress.dart';
 import 'package:gymnastic_center/infrastructure/camara_gallery/camara_gallery_manager.dart';
+import 'package:image_cropper/image_cropper.dart';
 import 'package:image_picker/image_picker.dart';
 
 class CamaraGalleryImpl extends CamaraGalleryManager {
   final ImagePicker _picker = ImagePicker();
+  final ImageCropper _recort = ImageCropper();
 
   @override
   Future<String?> selectPhoto() async {
@@ -14,23 +19,56 @@ class CamaraGalleryImpl extends CamaraGalleryManager {
     );
 
     if (photo == null) return null;
-    return _convertImageToBase64(photo);
+    final String? base64Image = await _recortImage(photo.path);
+    return base64Image;
   }
 
   @override
   Future<String?> takePhoto() async {
     final XFile? photo = await _picker.pickImage(
       source: ImageSource.camera,
-      imageQuality: 1,
+      imageQuality: 100,
       preferredCameraDevice: CameraDevice.rear,
     );
 
     if (photo == null) return null;
-    return _convertImageToBase64(photo);
+    final String? base64Image = await _recortImage(photo.path);
+    return base64Image;
   }
 
-  Future<String> _convertImageToBase64(XFile image) async {
-    final imageBytes = await image.readAsBytes();
-    return base64Encode(imageBytes);
+  Future<String> _converToBase64(Uint8List bytes) async {
+    return base64Encode(bytes);
+  }
+
+  Future<String?> _recortImage(String pathImage) async {
+    final CroppedFile? croppedFile = await _recort.cropImage(
+        cropStyle: CropStyle.circle,
+        sourcePath: pathImage,
+        compressQuality: 100,
+        aspectRatioPresets: [
+          CropAspectRatioPreset.ratio3x2
+        ],
+        uiSettings: [
+          IOSUiSettings(),
+          AndroidUiSettings(
+            cropGridColor: Colors.transparent,
+            cropFrameColor: Colors.transparent,
+            hideBottomControls: true,
+          ),
+        ]);
+
+    if (croppedFile == null) return null;
+    final bytes = await _compressFile(croppedFile.path);
+    if (bytes != null) return null;
+    return _converToBase64(bytes!);
+  }
+
+  Future<Uint8List?> _compressFile(String file) async {
+    return await FlutterImageCompress.compressWithFile(
+      file,
+      minWidth: 300,
+      minHeight: 300,
+      quality: 80,
+    );
   }
 }
