b8390687afddc4e855e2f42a442873bb479a3b03
HappyGick <egarciayustiz@gmail.com>
2024-06-14 01:42:56 -0400

    feat(reset_password): :lipstick: add bloc to verification code screen

--- lib/presentation/screens/auth/verification_code_screen.dart
+++ lib/presentation/screens/auth/verification_code_screen.dart
@@ -1,28 +1,42 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:gymnastic_center/application/auth/register/register_bloc.dart';
+import 'package:go_router/go_router.dart';
+import 'package:gymnastic_center/application/auth/recover_password/recover_password_bloc.dart';
 import 'package:gymnastic_center/application/themes/themes_bloc.dart';
+import 'package:gymnastic_center/infrastructure/datasources/user/api_user_datasource.dart';
+import 'package:gymnastic_center/infrastructure/local_storage/local_storage.dart';
+import 'package:gymnastic_center/infrastructure/repositories/user/user_repository_impl.dart';
 import 'package:gymnastic_center/presentation/widgets/shared/backgrounds/circle_masked_background.dart';
-import 'package:gymnastic_center/presentation/widgets/shared/checkbox_form_field.dart';
-import 'package:gymnastic_center/presentation/widgets/shared/gradient_text.dart';
-import 'package:gymnastic_center/presentation/widgets/shared/gymnastic_text_form_field/gymnastic_text_form_field.dart';
-import 'package:gymnastic_center/presentation/widgets/shared/gymnastic_text_form_field/gymnastic_text_input_decoration.dart';
 
-class VerificationCodeScreen extends StatefulWidget {
+class VerificationCodeScreen extends StatelessWidget {
   const VerificationCodeScreen({super.key});
 
   @override
-  VerificationCodeScreenState createState() => VerificationCodeScreenState();
+  Widget build(BuildContext context) {
+    return BlocProvider(
+      create: (context) => RecoverPasswordBloc(
+          userRespository: UserRepositoryImpl(
+        userDatasource: APIUserDatasource(),
+        keyValueStorage: LocalStorageService(),
+      )),
+      child: const _VerificationCodeScreen(),
+    );
+  }
 }
 
-class VerificationCodeScreenState extends State<VerificationCodeScreen> {
+class _VerificationCodeScreen extends StatefulWidget {
+  const _VerificationCodeScreen({super.key});
+
+  @override
+  _VerificationCodeScreenState createState() => _VerificationCodeScreenState();
+}
+
+class _VerificationCodeScreenState extends State<_VerificationCodeScreen> {
   late TextEditingController _firstDigit;
   late TextEditingController _secondDigit;
   late TextEditingController _thirdDigit;
   late TextEditingController _fourthDigit;
-
-  bool _hidePassword = true;
 
   final _formKey = GlobalKey<FormState>();
 
@@ -45,6 +59,25 @@ class VerificationCodeScreenState extends State<VerificationCodeScreen> {
     _fourthDigit.text = '\u200b';
   }
 
+  void _onCodeChanged() {
+    String firstDigit =
+        _firstDigit.text.length > 1 ? _firstDigit.text.substring(1) : '';
+    String secondDigit =
+        _secondDigit.text.length > 1 ? _secondDigit.text.substring(1) : '';
+    String thirdDigit =
+        _thirdDigit.text.length > 1 ? _thirdDigit.text.substring(1) : '';
+    String fourthDigit =
+        _fourthDigit.text.length > 1 ? _fourthDigit.text.substring(1) : '';
+
+    String code = firstDigit + secondDigit + thirdDigit + fourthDigit;
+
+    context.read<RecoverPasswordBloc>().changeCode(code);
+
+    if (code.length == 4) {
+      context.read<RecoverPasswordBloc>().validateCode();
+    }
+  }
+
   Widget _textFieldPadding(Widget child,
       {bool bottom = true, bool top = true}) {
     double paddingValue = MediaQuery.of(context).size.height * 0.022;
@@ -55,7 +88,7 @@ class VerificationCodeScreenState extends State<VerificationCodeScreen> {
   }
 
   Widget _digitTextField(TextEditingController controller, FocusNode focusNode,
-      {FocusNode? next, FocusNode? previous, Function()? onTypedAction}) {
+      {FocusNode? next, FocusNode? previous}) {
     double digitTextFieldSize = MediaQuery.of(context).size.width / 6;
     double digitTextFieldFontSize = MediaQuery.of(context).size.width * 0.072;
     double digitTextFieldLineHeight = 1.072;
@@ -72,7 +105,7 @@ class VerificationCodeScreenState extends State<VerificationCodeScreen> {
             }
             if (value.length > 1) {
               if (next != null) next.requestFocus();
-              if (onTypedAction != null) onTypedAction();
+              _onCodeChanged();
             }
           },
           maxLength: 2,
@@ -106,56 +139,77 @@ class VerificationCodeScreenState extends State<VerificationCodeScreen> {
     if (_formKey.currentState!.validate()) {
       // If the form is valid, display a snackbar. In the real world,
       // you'd often call a server or save the information in a database.
-      await context.read<RegisterBloc>().obSubmitRegister();
+      await context.read<RecoverPasswordBloc>().validateCode();
     }
   }
 
   @override
   Widget build(BuildContext context) {
-    return _layout([
-      _textFieldPadding(
-          const Text('Verification Code',
-              style: TextStyle(
-                  fontSize: 36,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.white)),
-          top: false),
-      _textFieldPadding(
-          const Text(
-            'Please type the verification code sent to youremail@gmail.com',
-            style: TextStyle(
-              color: Colors.white,
-              fontSize: 16,
-            ),
-            textAlign: TextAlign.center,
-          ),
-          top: false),
-      Row(mainAxisAlignment: MainAxisAlignment.spaceEvenly, children: [
-        _digitTextField(_firstDigit, _firstDigitFocusNode,
-            next: _secondDigitFocusNode),
-        _digitTextField(_secondDigit, _secondDigitFocusNode,
-            next: _thirdDigitFocusNode, previous: _firstDigitFocusNode),
-        _digitTextField(_thirdDigit, _thirdDigitFocusNode,
-            next: _fourthDigitFocusNode, previous: _secondDigitFocusNode),
-        _digitTextField(_fourthDigit, _fourthDigitFocusNode,
-            previous: _thirdDigitFocusNode),
-      ]),
-      Row(mainAxisAlignment: MainAxisAlignment.center, children: <Widget>[
-        const Text("I didn't receive a code!",
-            style: TextStyle(color: Colors.white, fontSize: 16)),
-        const SizedBox(
-          width: 10,
-        ),
-        GestureDetector(
-          onTap: () {},
-          child: const Text("Please resend",
-              style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 16,
-                  fontWeight: FontWeight.bold)),
-        ),
-      ]),
-    ]);
+    return BlocConsumer<RecoverPasswordBloc, RecoverPasswordState>(
+        listenWhen: (previous, current) =>
+            previous.formStatus != current.formStatus,
+        listener: (context, state) {
+          if (state.formStatus == RecoverPasswordFormStatus.invalid) {
+            ScaffoldMessenger.of(context).hideCurrentSnackBar();
+            ScaffoldMessenger.of(context).showSnackBar(
+              SnackBar(content: Text(state.errorMessage)),
+            );
+          }
+
+          if (state.formStatus == RecoverPasswordFormStatus.valid) {
+            ScaffoldMessenger.of(context).hideCurrentSnackBar();
+            context.go('/create_password');
+          }
+        },
+        builder: (context, state) => _layout([
+              _textFieldPadding(
+                  const Text('Verification Code',
+                      style: TextStyle(
+                          fontSize: 36,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white)),
+                  top: false),
+              _textFieldPadding(
+                  const Text(
+                    'Please type the verification code sent to youremail@gmail.com',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 16,
+                    ),
+                    textAlign: TextAlign.center,
+                  ),
+                  top: false),
+              Row(mainAxisAlignment: MainAxisAlignment.spaceEvenly, children: [
+                _digitTextField(_firstDigit, _firstDigitFocusNode,
+                    next: _secondDigitFocusNode),
+                _digitTextField(_secondDigit, _secondDigitFocusNode,
+                    next: _thirdDigitFocusNode, previous: _firstDigitFocusNode),
+                _digitTextField(_thirdDigit, _thirdDigitFocusNode,
+                    next: _fourthDigitFocusNode,
+                    previous: _secondDigitFocusNode),
+                _digitTextField(_fourthDigit, _fourthDigitFocusNode,
+                    previous: _thirdDigitFocusNode),
+              ]),
+              Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    const Text("I didn't receive a code!",
+                        style: TextStyle(color: Colors.white, fontSize: 16)),
+                    const SizedBox(
+                      width: 10,
+                    ),
+                    GestureDetector(
+                      onTap: () {
+                        context.read<RecoverPasswordBloc>().sendCode();
+                      },
+                      child: const Text("Please resend",
+                          style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 16,
+                              fontWeight: FontWeight.bold)),
+                    ),
+                  ]),
+            ]));
   }
 
   Widget _layout(List<Widget> children) {
