91f9395e247d22c20b5334da23534ea00f10b20f
Alejandro <alejoguevarafm@gmail.com>
2024-06-18 19:16:05 -0400

    perf: :zap: Blog improve
    Se ha segregado las funciones del bloc de Blogs ahora uno solo se encarga de los blogs y uno para buscar un blog en particular

--- lib/application/blogs/bloc/blogs_state.dart
+++ lib/application/blogs/bloc/blogs_state.dart
@@ -3,28 +3,24 @@ part of 'blogs_bloc.dart';
 enum BlogStatus { loading, error, loaded, allBlogsLoaded }
 
 class BlogsState extends Equatable {
-  final Blog currentBlog;
   final BlogStatus status;
   final List<Blog> loadedBlogs;
   final int page;
   final int perPage;
 
   const BlogsState(
-      {required this.currentBlog,
-      this.loadedBlogs = const [],
+      {this.loadedBlogs = const [],
       this.status = BlogStatus.loaded,
       this.page = 1,
       this.perPage = 10});
 
   BlogsState copyWith({
-    Blog? currentBlog,
     BlogStatus? status,
     List<Blog>? loadedBlogs,
     int? page,
     int? perPage,
   }) =>
       BlogsState(
-        currentBlog: currentBlog ?? this.currentBlog,
         status: status ?? this.status,
         loadedBlogs: loadedBlogs ?? this.loadedBlogs,
         page: page ?? this.page,
@@ -32,5 +28,5 @@ class BlogsState extends Equatable {
       );
 
   @override
-  List<Object> get props => [currentBlog, status, loadedBlogs];
+  List<Object> get props => [status, loadedBlogs];
 }
