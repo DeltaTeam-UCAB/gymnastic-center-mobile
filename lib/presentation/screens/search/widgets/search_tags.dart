2ee8777a978d5da6c2eac7eb7e3b5d01505ca697
Alejandro <alejoguevarafm@gmail.com>
2024-07-06 14:32:22 -0400

    feat: :sparkles: Search Screens & Widgets related
    Se realizo la screen de search a espera de una mejora luego de hacer merge, ademas se añadio el import en screens

---
+++ lib/presentation/screens/search/widgets/search_tags.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:gymnastic_center/application/themes/themes_bloc.dart';
+
+class SearchTags extends StatefulWidget {
+  final List<String> tags;
+  final void Function(List<String> selectedTags) onChangeTags;
+  const SearchTags(this.tags, {super.key, required this.onChangeTags});
+
+  @override
+  _SearchTagsState createState() => _SearchTagsState();
+}
+
+class _SearchTagsState extends State<SearchTags> {
+  final Set<String> _activeTags = {};
+
+  @override
+  Widget build(BuildContext context) {
+    final bool isDark = context.read<ThemesBloc>().isDarkMode; 
+    return Padding(
+      padding: const EdgeInsets.only(top: 10),
+      child: Wrap(
+        spacing: 9,
+        runSpacing: 9,
+        children: widget.tags.map((tag) {
+          final bool isActive = _activeTags.contains(tag);
+          return ElevatedButton(
+            onPressed: () {
+              setState(() {
+                if (isActive) {
+                  _activeTags.remove(tag);
+                } else {
+                  _activeTags.add(tag);
+                }
+                widget.onChangeTags(_activeTags.toList());
+              });
+            },
+            style: ElevatedButton.styleFrom(
+              backgroundColor: isActive
+                  ? isDark
+                      ? Colors.white
+                      : const Color.fromARGB(255, 93, 43, 187)
+                  : isDark
+                      ? Colors.grey.shade800
+                      : const Color.fromARGB(255, 226, 221, 239),
+              foregroundColor: isActive
+                  ? isDark
+                      ? Colors.deepPurple
+                      : Colors.white
+                  : isDark
+                      ? Colors.white
+                      : const Color.fromARGB(255, 93, 43, 187),
+            ),
+            child: Text(tag),
+          );
+        }).toList(),
+      ),
+    );
+  }
+}
