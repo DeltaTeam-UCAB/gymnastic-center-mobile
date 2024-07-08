53b9244e77ae68dcf7b68f71b6f0694939eeb403
Alejandro <alejoguevarafm@gmail.com>
2024-07-07 20:57:15 -0400

    perf: :zap: Infrastructure changes related to trainer
    Cambios importantes para el funcionamiento del trainer

--- lib/infrastructure/datasources/trainers/api_trainer_datasource.dart
+++ lib/infrastructure/datasources/trainers/api_trainer_datasource.dart
@@ -1,46 +1,72 @@
 import 'package:dio/dio.dart';
 import 'package:gymnastic_center/application/key_value_storage/key_value_storage.dart';
 import 'package:gymnastic_center/domain/datasources/trainers/trainers_datasource.dart';
-import 'package:gymnastic_center/domain/entities/courses/course.dart';
 import 'package:gymnastic_center/infrastructure/core/constants/environment.dart';
-import 'package:gymnastic_center/infrastructure/mappers/course_mapper.dart';
 import 'package:gymnastic_center/infrastructure/mappers/trainer_mapper.dart';
-import 'package:gymnastic_center/infrastructure/models/courses/course_response.dart';
 import 'package:gymnastic_center/infrastructure/models/trainer/trainer_response.dart';
 
 class ApiTrainersDatasource extends TrainersDataSource {
   final KeyValueStorageService keyValueStorage;
-  final dio = Dio(BaseOptions(baseUrl: '${Environment.backendApi}/trainer'));
+  final dio = Dio(BaseOptions(baseUrl: Environment.backendApi));
 
-  ApiTrainersDatasource(this.keyValueStorage){
-    dio.interceptors.add(InterceptorsWrapper(
-      onRequest: (options, handler) async {
-        final token = await keyValueStorage.getValue<String>('token');
-        options.headers['Authorization'] = 'Bearer $token';
-        return handler.next(options);
-      }
-    ));
+  ApiTrainersDatasource(this.keyValueStorage) {
+    dio.interceptors
+        .add(InterceptorsWrapper(onRequest: (options, handler) async {
+      final token = await keyValueStorage.getValue<String>('token');
+      options.headers['Authorization'] = 'Bearer $token';
+      return handler.next(options);
+    }));
   }
 
-  Future<Course> getCourseById(String id) async {
-    final response = await dio.get('/course/one/$id');
-    final courseResponse = CourseResponse.fromJson(response.data);
-    return CourseMapper.courseToEntity(courseResponse);
+  Future<int> _getBlogCount(String trainerId) async {
+    final response = await dio.get('/blog/count?trainer=$trainerId');
+    return response.data['count'];
   }
-  
+
+  Future<int> _getCourseCount(String trainerId) async {
+    final response = await dio.get('/course/count?trainer=$trainerId');
+    return response.data['count'];
+  }
+
   @override
   Future<TrainerDetails> getTrainerById(String trainerId) async {
-    final response = await dio.get('/one/$trainerId');
+    final blogCount = await _getBlogCount(trainerId);
+    final courseCount = await _getCourseCount(trainerId);
+
+    final response = await dio.get('/trainer/one/$trainerId');
     final trainerResponse = TrainerResponse.fromJson(response.data);
+
     return TrainerDetails(
-      trainer: TrainerMapper.trainerToEntity(trainerResponse), 
-      isFollowing: trainerResponse.isFollowed
-    );
+        trainer: TrainerMapper.trainerToEntity(trainerResponse),
+        isFollowing: trainerResponse.isFollowed,
+        blogCount: blogCount,
+        courseCount: courseCount);
   }
-  
+
   @override
   Future<bool> toggleFollow(String trainerId) async {
-    final response = await dio.post('/toggle/follow/$trainerId');
+    final response = await dio.post('/trainer/toggle/follow/$trainerId');
     return response.data['userFollow'];
+  }
+
+  @override
+  Future<List<TrainerDetails>> getTrainersPaginated(
+      {int page = 1, int perPage = 10, bool filterByFollowed = false}) async {
+    final response = await dio.get(
+      '/trainer/many',
+      queryParameters: {
+        'page': page,
+        'perPage': perPage,
+        'filterByFollowed': filterByFollowed,
+      },
+    );
+
+    final trainers = (response.data as List).map((data) {
+      final apiTrainerResponse = TrainerResponse.fromJson(data);
+      final trainer = TrainerMapper.trainerToEntity(apiTrainerResponse);
+      return TrainerDetails(trainer: trainer, isFollowing: apiTrainerResponse.isFollowed);
+    }).toList();
+
+    return trainers;
   }
 }
--- lib/infrastructure/models/trainer/trainer_response.dart
+++ lib/infrastructure/models/trainer/trainer_response.dart
@@ -1,24 +1,29 @@
 class TrainerResponse {
   final String id;
   final String name;
-  final String locations;
+  final String location;
   final int followers;
   final bool isFollowed;
+  final String image;
 
   TrainerResponse({
     required this.id,
     required this.name,
-    this.locations = '',
+    this.location = '',
     this.followers = 0,
     this.isFollowed = false,
+    this.image =
+        'https://cdn.icon-icons.com/icons2/3551/PNG/512/trainer_man_people_avatar_person_icon_224850.png',
   });
 
   factory TrainerResponse.fromJson(Map<String, dynamic> json) =>
       TrainerResponse(
         id: json["id"],
         name: json["name"],
-        locations: json["locations"] ?? '',
+        location: json["location"] ?? '',
         followers: json["followers"] ?? 0,
         isFollowed: json["userFollow"] ?? false,
+        image: json["image"] ??
+            'https://cdn.icon-icons.com/icons2/3551/PNG/512/trainer_man_people_avatar_person_icon_224850.png',
       );
 }
