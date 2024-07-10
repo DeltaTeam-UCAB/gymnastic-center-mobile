8f06304a74d8b1d73fcf2e2da45a57b9b794ba04
HappyGick <egarciayustiz@gmail.com>
2024-07-09 22:15:15 -0400

    feat(cache): :label: add CacheRetriever

---
+++ lib/infrastructure/cache/core/cache_retriever.dart
@@ -0,0 +1,27 @@
+import 'package:gymnastic_center/common/optional.dart';
+import 'package:gymnastic_center/infrastructure/cache/core/cache_provider.dart';
+import 'package:internet_connection_checker_plus/internet_connection_checker_plus.dart';
+
+class CacheRetriever {
+  CacheProvider cacheProvider;
+
+  CacheRetriever(this.cacheProvider);
+
+  Future<T> retrieve<T>(
+      {required Future<T> Function() truthSource,
+      required String collection,
+      required String identifier}) async {
+    if (await InternetConnection().hasInternetAccess) {
+      var data = await truthSource();
+      await cacheProvider.write(data, collection, identifier);
+      return data;
+    } else {
+      Optional<T> cacheData = await cacheProvider.read(collection, identifier);
+      if (cacheData.hasValue()) {
+        return cacheData.getValue();
+      }
+    }
+    throw Exception(
+        'Could not get data, check your connection or try again later');
+  }
+}
